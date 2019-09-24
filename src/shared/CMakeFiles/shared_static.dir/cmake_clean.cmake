601b3871d7f36bbd0969662b906d73439cfb1273
moses <beugremoses@gmail.com>
2019-09-24 17:08:21 +0200

    Updating after downloading python

---
+++ src/shared/CMakeFiles/shared_static.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shared_static.dir/state/Exemple.cpp.o"
+  "libshared_static.pdb"
+  "libshared_static.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shared_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test/client/CMakeFiles/test_client_dummy.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ensea/plt/test/client/test_client_dummy.cpp" "/home/ensea/plt/test/client/CMakeFiles/test_client_dummy.dir/test_client_dummy.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_TEST_DYN_LINK"
+  "BOOST_TEST_MAIN"
+  "BOOST_TEST_MODULE=client_dummy"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src/shared"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ensea/plt/src/shared/CMakeFiles/shared_static.dir/DependInfo.cmake"
+  "/home/ensea/plt/src/client/CMakeFiles/client_static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
