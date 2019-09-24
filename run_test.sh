a97fc86f81f567611b5e8e86060be4c279ad94ed
moses <beugremoses@gmail.com>
2019-09-24 17:08:21 +0200

    Updating

---
+++ run_test.sh
@@ -0,0 +1,2 @@
+cd "/home/ensea/plt/test"
+ctest
---
+++ test/shared/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ensea/plt/test/shared
+# Build directory: /home/ensea/plt/test/shared
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_shared_dummy "/home/ensea/plt/test/shared/test_shared_dummy")
