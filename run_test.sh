642a351790789032326291f64245c0caf90fadc1
moses <beugremoses@gmail.com>
2019-10-08 09:46:20 +0200

    Cleanup build system
    Remove CMake generated files.

--- run_test.sh
+++
@@ -1,2 +0,0 @@
-cd "/home/ensea/plt/test"
-ctest
--- test/client/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ensea/plt/test/client
-# Build directory: /home/ensea/plt/test/client
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_client_dummy "/home/ensea/plt/test/client/test_client_dummy")
