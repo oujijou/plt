8b88b859d7ad04ac3454f71354fe5a564a668dbf
moses <beugremoses@gmail.com>
2019-10-21 11:48:20 +0200

    Rename the images files PNG->png

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -4,7 +4,34 @@
 #include <SFML/Graphics.hpp>
 
 void testSFML() {
+    
     sf::Texture texture;
+    if (!texture.loadFromFile("/home/ensea/plt/res/5.png"))
+    {
+        std::cout<<"Load Failed" << std::endl;
+        system("Pause");
+    }
+    
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!",sf::Style::Default);
+    // sf::CircleShape shape(100.f);
+    // shape.setFillColor(sf::Color::Red);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
 }
 
 // Fin test SFML
@@ -17,6 +44,7 @@ using namespace state;
 int main(int argc,char* argv[])
 {
     cout << "Bonjour " <<argv[1] << endl;
+    testSFML();
 
     return 0;
 }
