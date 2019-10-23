8e9a43cfef441fc2faca14b2aa9551801449658f
moses <beugremoses@gmail.com>
2019-10-23 23:49:07 +0200

    display one fighter on arena in main file

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -12,29 +12,49 @@ void testSFML() {
     int row = 0;   
     int frameCounter = 0;
 
-    sf::Texture texture;
-    sf::Sprite texSprite;
-    if (!texture.loadFromFile("/home/ensea/plt/res/arena1.jpg"))
+    //display arena
+    sf::Texture arena;
+    sf::Sprite arenaSprite;
+    sf:: Vector2u arenaSize = arena.getSize();
+    if (!arena.loadFromFile("/home/ensea/plt/res/arena1.png"))
     {
         std::cout<<"Load Failed" << std::endl;
         system("Pause");
     }
-    texSprite.setTexture(texture);
-
+    arenaSprite.setTexture(arena);
+    
+    //display fighter1
     sf::Texture spriteSheet;
-    if (!spriteSheet.loadFromFile("/home/ensea/plt/res/ken.png",sf::IntRect(7,7,100,120)))
+    sf:: IntRect rectSourceSprite(0,0,100,120);
+    if (!spriteSheet.loadFromFile("/home/ensea/plt/res/ken.png",rectSourceSprite));
     {
         std::cout<<"Load Failed" << std::endl;
         system("Pause");
     }
     sf::Sprite playerSprite;
     playerSprite.setTexture(spriteSheet,true);
-    //playerSprite.setScale(0.5,0.5);
+    // put fighter on the right of the arena
+    playerSprite.setPosition(sf::Vector2f(500.f, 210.f)); 
     
 
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!",sf::Style::Default);
-    // sf::CircleShape shape(100.f);
-    // shape.setFillColor(sf::Color::Red);
+    //display fighter2
+    // sf::Texture spriteSheet2;
+    // //sf:: IntRect rectSourceSprite1(0,0,100,120);
+    // if (!spriteSheet.loadFromFile("/home/ensea/plt/res/billylee.png"))
+    // {
+    //     std::cout<<"Load Failed" << std::endl;
+    //     system("Pause");
+    // }
+    // sf::Sprite playerSprite2;
+    // playerSprite2.setOrigin(10.f, 20.f);
+    // playerSprite2.setTexture(spriteSheet2,true);
+    // // put fighter on the left of the arena
+    // playerSprite2.setPosition(sf::Vector2f(100.f, 210.f)); 
+
+    //size of image as input of VideoMode
+    sf::RenderWindow window(sf::VideoMode(640, 384), "SFML works!",sf::Style::Default);
+    
+    sf::Clock clock;
 
     while (window.isOpen())
     {
@@ -44,12 +64,24 @@ void testSFML() {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+        // if(clock.getElapsedTime().asSeconds()>1.0f)
+        // {
+        //     if(rectSourceSprite.left==300)
+        //         rectSourceSprite.left=0;
+        //     else
+        //     {
+        //         rectSourceSprite.left +=100;
+        //     }
+        //     playerSprite.setTextureRect(rectSourceSprite);
+        //     clock.restart();
+            
+        
         //playerSprite.setTextureRect(sf::IntRect(32*frame, row, 32,48));
 
         window.clear();
-        window.draw(texSprite);
+        window.draw(arenaSprite);
         window.draw(playerSprite);
+        // window.draw(playerSprite2);
         window.display();
     }
 }
