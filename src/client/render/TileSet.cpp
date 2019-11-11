3c47d60441651b6d8d1b113fb2332e68700b82ec
Ouajih <oujijou@outlook.fr>
2019-11-11 16:52:45 +0100

    update render, TileSet.cpp and add Command.cpp

--- src/client/render/TileSet.cpp
+++ src/client/render/TileSet.cpp
@@ -3,6 +3,55 @@
 using namespace std;
 using namespace render;
 
+
+TileSet::TileSet(tileSetID newID){
+	id = newID;
+	if (id == KUROTILE )
+     {
+         imageFile="/home/ensea/plt/res/Fighters/Kuro.png";
+         textureTileSet.loadFromFile(imageFile);
+     }
+     if (id == FLINTTILE)
+     {
+         imageFile="/home/ensea/plt/res/Fighters/Flint.png";
+         textureTileSet.loadFromFile(imageFile);
+     }
+     if (id == THORKTILE)
+     {
+         imageFile="/home/ensea/plt/res/Fighters/Thork.png";
+         textureTileSet.loadFromFile(imageFile);
+     }
+     if (id == SEKUTILE)
+     {
+         imageFile="/home/ensea/plt/res/Fighters/Seku.png";
+         textureTileSet.loadFromFile(imageFile);
+     }
+     if (id == FLINTTERRAIN )
+     {
+         imageFile="/home/ensea/plt/res/Terrains/flint_terrain.png";
+         textureTileSet.loadFromFile(imageFile);
+     }
+      if (id == SEKUTERRAIN )
+     {
+         imageFile="/home/ensea/plt/res/Terrains/seku_terrain.png";
+         textureTileSet.loadFromFile(imageFile);
+     }
+      if (id == THORKTERRAIN )
+     {
+         imageFile="/home/ensea/plt/res/Terrains/thork_terrain.png";
+         textureTileSet.loadFromFile(imageFile);
+     }
+      if (id == KUROTERRAIN )
+     {
+         imageFile="/home/ensea/plt/res/Terrains/kuro_terrain.png";
+         textureTileSet.loadFromFile(imageFile);
+     }
+	
+}
+
+sf::Texture& TileSet::getTexture(){
+	return textureTileSet;
+}
 // TileSet::TileSet(TileSetID newID){
 //     id = newID;
 //     sf:: IntRect rectSourceSprite(0,0,100,100);
