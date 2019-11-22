86a76591ae1e0d59db569a19291af3312b68e058
Ouajih <oujijou@outlook.fr>
2019-11-22 17:53:38 +0100

    update defenseCommand and engine

--- src/shared/engine/DefenseCommand.cpp
+++ src/shared/engine/DefenseCommand.cpp
@@ -12,77 +12,19 @@ using namespace state;
 
 //Constructor
 
-DefenseCommand::DefenseCommand (state::Fighter& fighter):fighter(fighter)
-{
+ DefenseCommand::DefenseCommand (state::Fighter& isDefending):isDefending(isDefending)
+ {
 
-}
-//Functions
-void DefenseCommand::execute (state::State& state){
+ }
+ //Functions
+ void DefenseCommand::execute (state::State& state){
     
     
-    if(attacker.getStatus()!=DEAD){
+     if(isDefending.getStatus()!=DEAD){
 		
-		cout<<"Defense is coming!!"<<endl;
-        float oldTargetHealth=target.getHealthPoints();
-        
-        //Target
-        int waitingTime=40000;
-		string entityNameTarget = "";
-		switch(attacker.getName()){
-			case Flint: 
-				entityNameTarget = "flint";
-            break;
-			case Kuro:
-				entityNameTarget = "kuro";
-            break;
-			case Thork:
-				entityNameTarget = "thork";
-            break;
-			case Seku:
-				entityNameTarget = "seku";
-            break;       
-    }
-
-        //Attack
-        attacker.fight(target, COUPDEPOING); //for now only this attack is available
-       
-		string entityNameAttacker = "";
-		switch(attacker.getName()){
-			case Flint: 
-				entityNameAttacker = "flint";
-            break;
-        case Kuro:
-            entityNameAttacker = "kuro";
-            break;
-        case Thork:
-            entityNameAttacker = "thork";
-            break;
-        case Seku:
-            entityNameAttacker = "seku";
-            break;
-        cout << "The " << entityNameAttacker << " will attack or try to attack the " << entityNameTarget << "." << endl;
-           
-    }
-        
-        cout<<"Defense is coming!!"<<endl;
-        float oldMana=fighter.getMana();
-        
-        int waitingTime=40000;
-		string entityNameRecharging = "";
-		switch(fighter.getName()){
-			case Flint: 
-				entityNameRecharging = "flint";
-            break;
-			case Kuro:
-				entityNameRecharging = "kuro";
-            break;
-			case Thork:
-				entityNameRecharging = "thork";
-            break;
-			case Seku:
-				entityNameRecharging = "seku";
-            break;       
-    }
-    fighter.defend(fighter);
-}
-}
+ 		cout<<"Defense is coming!!"<<endl;
+         
+     isDefending.defend(isDefending);
+		cout << "Le fighter est en mode" << isDefending.getStatus() << endl;
+ }
+ }
